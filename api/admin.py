5848c155bdc89a71b61a3a2dfdc8bcdd56738eaa
LearnerBoy443 <cabhirup69@gmail.com>
2026-07-16 20:00:32 +0530

    Delete api directory

--- api/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import UserProfile, ChatSession, ChatMessage, ImageMemory, DatabaseFile
-
-admin.site.register(UserProfile)
-admin.site.register(ChatSession)
-admin.site.register(ChatMessage)
-admin.site.register(ImageMemory)
-admin.site.register(DatabaseFile)
