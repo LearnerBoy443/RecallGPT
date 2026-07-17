d1b5faf57a9e34bb06030479bba301803fa32c0c
LearnerBoy443 <cabhirup69@gmail.com>
2026-07-17 20:29:48 +0530

    Add files via upload

---
+++ recall_project/urls.py
@@ -0,0 +1,47 @@
+"""
+URL configuration for recall_project project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app import views as other_views
+    2. Add a URL to urlpatterns:  path('', other_views.Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+"""
+URL configuration for recall_project project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app import views as other_views
+    2. Add a URL to urlpatterns:  path('', other_views.Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from api import views as api_views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    # Custom logout MUST override the built-in path directly at /logout/
+    path('logout/', api_views.logout_view, name='logout'),
+    path('', include('django.contrib.auth.urls')),
+    path('', include('api.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
